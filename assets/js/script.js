0da3d193b52e4910392ce28925b97e5785f5e404
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-09-10 12:45:18 +0100

    Subtraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,21 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+    
     runGame("addition");
 });
     
 /**Main game loop, runs at start and when the user has answered a question */
 function runGame(gameType)
 {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
