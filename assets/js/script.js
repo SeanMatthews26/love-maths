406662f8fa37de11a533956abad4a3d3e4b5235a
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-09-10 12:15:27 +0100

    Multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,10 @@ function runGame(gameType)
     {
         displayAdditionQuestion(num1, num2);
     }
+    else if(gameType === "multiply")
+    {
+        displayMultiplyQuestion(num1, num2);
+    }
     else
     {
         alert(`Unknown game type: ${gameType}`);
@@ -61,6 +65,10 @@ function calculateCorrectAnswer()
     {
         return [operand1 + operand2, "addition"];
     }
+    else if(operator === "x")
+    {
+        return [operand1 * operand2, "multiply"];
+    }
     else
     {
         alert(`Unimplemented operator ${operator}`);
@@ -92,7 +100,9 @@ function displaySubtractQuestion()
 
 }
 
-function displayMultiplyQuestion()
+function displayMultiplyQuestion(operand1, operand2)
 {
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
