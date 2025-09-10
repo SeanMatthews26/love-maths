47ca0d65d093b539ef92f8db27746f74249bb443
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-09-10 12:27:44 +0100

    subtraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,10 @@ function runGame(gameType)
     {
         displayMultiplyQuestion(num1, num2);
     }
+    else if(gameType === "subtract")
+    {
+        displaySubtractQuestion(num1, num2);
+    }
     else
     {
         alert(`Unknown game type: ${gameType}`);
@@ -69,6 +73,10 @@ function calculateCorrectAnswer()
     {
         return [operand1 * operand2, "multiply"];
     }
+    else if(operator === "-")
+    {
+        return [operand1 - operand2, "subtract"];
+    }
     else
     {
         alert(`Unimplemented operator ${operator}`);
@@ -95,9 +103,11 @@ function displayAdditionQuestion(operand1, operand2)
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion()
+function displaySubtractQuestion(operand1, operand2)
 {
-
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2)
