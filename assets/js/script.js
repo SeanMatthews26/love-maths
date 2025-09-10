3d50b4f96c4adc8a70021d2de7e638c84e923624
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-09-10 10:41:11 +0100

    Display Addition Question

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
     
 /**Main game loop, runs at start and when the user has answered a question */
-function runGame()
+function runGame(gameType)
 {
     //Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-}
 
-runGame();
+    if (gameType === "addition")
+    {
+        displayAdditionQuestion(num1, num2);
+    }
+    else
+    {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+}
 
 function checkAnswer()
 {
@@ -42,9 +52,11 @@ function incrementWrongAnswer()
 
 }
 
-function displayAdditionQuestion()
+function displayAdditionQuestion(operand1, operand2)
 {
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion()
